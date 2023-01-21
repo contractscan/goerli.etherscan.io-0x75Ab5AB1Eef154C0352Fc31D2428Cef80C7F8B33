e702e9d8fe5c2639c29d564183bf576f8b48a195
rabbit0222 <phamtranminhconghh92@gmail.com>
2023-01-21 13:59:34 +0000

    init repo

---
+++ IMultiFeeDistribution.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.6.12;
+
+interface IMultiFeeDistribution {
+
+    function addReward(address rewardsToken) external;
+    function mint(address user, uint256 amount, bool withPenalty) external;
+
+}
---
+++ IOnwardIncentivesController.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.6.12;
+
+interface IOnwardIncentivesController {
+    function handleAction(
+        address _token,
+        address _user,
+        uint256 _balance,
+        uint256 _totalSupply
+    ) external;
+}
